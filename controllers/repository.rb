ce0e67ff021d88031202d91336d03d661811f220
rjollet <renaudjollet@gmail.com>
2016-11-20 18:13:26 +0800

    display repositories services
    Signed-off-by: rjollet <renaudjollet@gmail.com>

--- controllers/repository.rb
+++ controllers/repository.rb
@@ -3,19 +3,13 @@
 # configure based on environment
 class DevRankAPI < Sinatra::Base
   get "/#{API_VER}/dev/:username/repos/?" do
-    developer_name = params[:username]
-    begin
-      dev = Developer.find(name: developer_name)
-      if !dev.nil?
-        content_type 'application/json'
-        DeveloperRepositoriesRepresenter.new(dev).to_json
-      else
-        error = Error.new(:not_found, "Cannot find Username: #{developer_name} repos")
-        return ErrorRepresenter.new(error).to_status_response
-      end
-    rescue
-      error = Error.new(:not_found, "Cannot find Username: #{developer_name} repos")
-      return ErrorRepresenter.new(error).to_status_response
+    results = DisplayRepositories.call(params)
+
+    if results.success?
+      content_type 'application/json'
+      DeveloperRepositoriesRepresenter.new(results.value).to_json
+    else
+      ErrorRepresenter.new(results.value).to_status_response
     end
   end
 end
